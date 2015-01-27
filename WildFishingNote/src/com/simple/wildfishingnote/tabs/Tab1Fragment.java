d5417777a7efdfa36f2fbff13e47bece7528aead
unknown <li_yang@microad-tech.com>
2015-01-27 21:46:08 +0800

    add android bootstrap library

--- WildFishingNote/src/com/simple/wildfishingnote/tabs/Tab1Fragment.java
+++ WildFishingNote/src/com/simple/wildfishingnote/tabs/Tab1Fragment.java
@@ -28,42 +28,42 @@ public class Tab1Fragment extends Fragment {
 		String historyDate = intent.getStringExtra(CalendarDailogActivity.HISTORY_DATE);
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_tab1, container, false);
-        TextView tv = (TextView)view.findViewById(R.id.textView2);
-        tv.setText(historyDate);
+//        TextView tv = (TextView)view.findViewById(R.id.textView2);
+//        tv.setText(historyDate);
         
-        initListView(view);
+//        initListView(view);
         
         return view;
     }
 	
-	private void initListView(View view) {
-		ListView lv = (ListView) view.findViewById(R.id.listView1);
-
-        String[] data = { "第1讲", "第2讲", "第3讲", "第4讲", "第5讲", "第6讲", "第7讲", "第8讲","第8讲","第10讲" };
-
-        lv.setAdapter(new ArrayAdapter(view.getContext(),
-                android.R.layout.simple_list_item_1, data));
-
-        lv.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id) {
-                Toast.makeText(view.getContext(),
-                        "您点击的是：" + ((TextView) view).getText(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        lv.setOnItemLongClickListener(new OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view,
-                    int position, long id){
-                Toast.makeText(view.getContext(),
-                        "您长按的是："+((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-	}
+//	private void initListView(View view) {
+//		ListView lv = (ListView) view.findViewById(R.id.listView1);
+//
+//        String[] data = { "第1讲", "第2讲", "第3讲", "第4讲", "第5讲", "第6讲", "第7讲", "第8讲","第8讲","第10讲" };
+//
+//        lv.setAdapter(new ArrayAdapter(view.getContext(),
+//                android.R.layout.simple_list_item_1, data));
+//
+//        lv.setOnItemClickListener(new OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                    int position, long id) {
+//                Toast.makeText(view.getContext(),
+//                        "您点击的是：" + ((TextView) view).getText(),
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        lv.setOnItemLongClickListener(new OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view,
+//                    int position, long id){
+//                Toast.makeText(view.getContext(),
+//                        "您长按的是："+((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        });
+//	}
 	
 	
 
