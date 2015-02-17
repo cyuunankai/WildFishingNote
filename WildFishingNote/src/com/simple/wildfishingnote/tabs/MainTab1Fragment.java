bba68e6806b74327841fe1e72a3712d58428ebb3
li_yang <li_yang@microad-tech.com>
2015-02-17 10:38:01 +0800

    add maintab1 - layout complete

--- WildFishingNote/src/com/simple/wildfishingnote/tabs/MainTab1Fragment.java
+++ WildFishingNote/src/com/simple/wildfishingnote/tabs/MainTab1Fragment.java
@@ -3,24 +3,24 @@ package com.simple.wildfishingnote.tabs;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.holoeverywhere.widget.Toast;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.simple.wildfishingnote.R;
@@ -31,7 +31,7 @@ import com.simple.wildfishingnote.sectionedlistview.SectionListAdapter;
 import com.simple.wildfishingnote.sectionedlistview.SectionListItem;
 import com.simple.wildfishingnote.sectionedlistview.SectionListView;
 
-public class MainTab1Fragment extends Fragment implements OnClickListener {
+public class MainTab1Fragment extends Fragment {
 
     private StandardArrayAdapter arrayAdapter;
     private SectionListAdapter sectionAdapter;
@@ -39,111 +39,44 @@ public class MainTab1Fragment extends Fragment implements OnClickListener {
     
     private View tab1View;
     
-    private static final float defaultFontSize = 20.0f;
-
-    private FlowTextView flowTextView;
-    private TextView dateTextView;
-    
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 	    
         tab1View = inflater.inflate(R.layout.activity_main_tab1, container, false);
         
-        setAddBtn1();
-        setAddBtn();
-        setDelBtn();
-        setShowBtn();
-        
-        dateTextView = (TextView) tab1View.findViewById(R.id.dateTextView);
-        flowTextView = (FlowTextView) tab1View.findViewById(R.id.ftv);
-        Spanned html = Html.fromHtml("阿呆家附近啊东风大街噶较高的风景郭德纲风刀霜剑公司东风股份等施工水电费井冈山的风景哥电风扇高富帅递归算法归属感阿呆家附近啊东风大街噶较高的风景郭德纲风刀霜剑公司东风股份等施工水电费井冈山的风景哥电风扇高富帅递归算法归属感");
-        flowTextView.setText(html);
-        
         List<SectionListItem> list = new ArrayList<SectionListItem>();
-        for(int i =0; i< 20; i++){
-            String section = "A";
+        for(int i =1; i< 20; i++){
+            String section = "2015-01";
+            String date = "2015-01-" + i;
+            String showImageFlag = "1";
             if(i > 10){
-                section = "B";
+                section = "2015-02";
+                date = "2015-02-" + i;
+                showImageFlag = "0";
             }
             Point p = new Point();
             p.setId(String.valueOf(i));
-            p.setDepth(" xxxxxxxxx" + i);
+            p.setDepth("title" + i);
+            p.setRodLengthName(date);
+            p.setLureMethodName("哦那都佛啊个网购耳光阿迪浓缩机嘎达给那都嘎达爱唯欧工二娃狗儿阿济格阿公阿二舅公二进宫而奥迪将公安违规阿拉斯蒂芬大驾光临大家阿斯顿飞骄傲就改为违规如果都是垃圾嘎达给解答搜狗价位给饥饿感");
+            p.setRodLengthId(showImageFlag);
             
             SectionListItem sli = new SectionListItem(p, section);
             list.add(sli);
         }
         
         
-//        arrayAdapter = new StandardArrayAdapter(getActivity(), list);
-//        sectionAdapter = new SectionListAdapter(inflater, arrayAdapter);
-//        listView = (SectionListView)tab1View.findViewById(R.id.section_list_view);
-//        listView.setAdapter(sectionAdapter);
+        arrayAdapter = new StandardArrayAdapter(getActivity(), list);
+        sectionAdapter = new SectionListAdapter(inflater, arrayAdapter);
+        listView = (SectionListView)tab1View.findViewById(R.id.section_list_view);
+        listView.setAdapter(sectionAdapter);
         
         
         
         return tab1View;
     }
-	
-	private void setAddBtn1() {
-        Button buttonCampaignPlacePre = (Button)tab1View.findViewById(R.id.set);
-        buttonCampaignPlacePre.setOnClickListener(this);
-    }
-	
-    private void setAddBtn() {
-        Button buttonCampaignPlacePre = (Button)tab1View.findViewById(R.id.addxx);
-        buttonCampaignPlacePre.setOnClickListener(this);
-    }
-    
-    private void setDelBtn() {
-        Button buttonCampaignPlacePre = (Button)tab1View.findViewById(R.id.delxx);
-        buttonCampaignPlacePre.setOnClickListener(this);
-    }
-    
-    private void setShowBtn() {
-        Button buttonCampaignPlacePre = (Button)tab1View.findViewById(R.id.showxx);
-        buttonCampaignPlacePre.setOnClickListener(this);
-    }
-	
-	@Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.set:
-                set();
-                
-                break;
-            case R.id.addxx:
-                increaseFontSize();
-                
-                break;
-            case R.id.delxx:
-                decreaseFontSize();
-                break;
-            case R.id.showxx:
-                show();
-                break;
-        }
-    }
-	
-	private void set(){
-	    EditText et = (EditText) tab1View.findViewById(R.id.setText);
-        dateTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, Float.valueOf(et.getText().toString()));
-    }
-	
-	private void increaseFontSize(){
-        float currentFontSize = flowTextView.getTextsize();
-        flowTextView.setTextSize(currentFontSize + 1.0f);
-    }
 
-    private void decreaseFontSize(){
-        float currentFontSize = flowTextView.getTextsize();
-        flowTextView.setTextSize(currentFontSize - 1.0f);
-    }
-
-    private void show(){
-        float currentFontSize = flowTextView.getTextsize();
-        Toast.makeText(getActivity(), "" + currentFontSize, Toast.LENGTH_SHORT).show();
-    }
 	
 	public class StandardArrayAdapter extends ArrayAdapter<SectionListItem> {
 
@@ -151,15 +84,17 @@ public class MainTab1Fragment extends Fragment implements OnClickListener {
         private final Activity context;
 
         public StandardArrayAdapter(Activity context, List<SectionListItem> list) {
-            super(context, R.layout.activity_place_listview_each_item, list);
+            super(context, R.layout.section_list_view_each_item, list);
             this.context = context;
             this.list = list;
         }
 
         class ViewHolder {
-            protected ImageView thumbnail_view;
-            protected TextView message_view;
-            protected FlowTextView ftv;
+            
+            protected TextView titleTextView;
+            protected FlowTextView summaryFlowTextView;
+            protected TextView dateTextView;
+            protected ImageView imageView;
         }
 
         /**`
@@ -177,10 +112,11 @@ public class MainTab1Fragment extends Fragment implements OnClickListener {
             if (convertView == null) {
                 // 添加UI到convertView
                 convertView = context.getLayoutInflater().inflate(R.layout.section_list_view_each_item, null);
-                viewHolder.ftv = (FlowTextView)convertView.findViewById(R.id.ftv);
-//                viewHolder.thumbnail_view = (ImageView)convertView.findViewById(R.id.thumbnail_view);
-//                viewHolder.message_view = (TextView)convertView.findViewById(R.id.message_view);
-//                viewHolder.message_view.setTag(list.get(position).item);//// 保存bean值到UI tag (响应事件从这个UI tag取值)
+                viewHolder.titleTextView = (TextView)convertView.findViewById(R.id.titleTextView);
+                viewHolder.summaryFlowTextView = (FlowTextView)convertView.findViewById(R.id.summaryFlowTextView);
+                viewHolder.dateTextView = (TextView)convertView.findViewById(R.id.dateTextView);
+                viewHolder.imageView = (ImageView)convertView.findViewById(R.id.imageView);
+                viewHolder.titleTextView.setTag(list.get(position).item);//// 保存bean值到UI tag (响应事件从这个UI tag取值)
                 convertView.setTag(viewHolder);
                 
                 // 添加事件
@@ -189,21 +125,32 @@ public class MainTab1Fragment extends Fragment implements OnClickListener {
             }
 
             // 设置bean值到UI
+            SpannableString spanString = new SpannableString(((Point)list.get(position).item).getDepth());
+            spanString.setSpan(new StyleSpan(Typeface.BOLD), 0, spanString.length(), 0);
+            viewHolder.titleTextView.setText(spanString);
             
-//            viewHolder.message_view.setText(((Point)list.get(position).item).getId());
+            viewHolder.dateTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8.0f);
+            viewHolder.dateTextView.setText(((Point)list.get(position).item).getRodLengthName());
             
-            String text = "阿呆家附近啊东风大街噶较高的风景郭德纲风刀霜剑公司东风股份等施工水电费井冈山的风景哥电风扇高富帅递归算法归属感";
-
-//            Display display = getActivity().getWindowManager().getDefaultDisplay();
-//            FlowTextHelper.tryFlowText(text, viewHolder.thumbnail_view, viewHolder.message_view, display);
+            String summary = ((Point)list.get(position).item).getLureMethodName();
+            Spanned html = Html.fromHtml(summary);
+            viewHolder.summaryFlowTextView.setText(html);
+            viewHolder.summaryFlowTextView.setTextSize(24.0f);
             
-            Spanned html = Html.fromHtml(text);
-//            flowTextView.setText(html);
-            viewHolder.ftv.setTextSize(25);
-            viewHolder.ftv.invalidate();
-            viewHolder.ftv.setText(html);
-//            viewHolder.example_text_view1.setText(((Point)list.get(position).item).getDepth());
-//            viewHolder.message_view.setTag(list.get(position).item);//// 保存bean值到UI tag (响应事件从这个UI tag取值)
+            
+            
+            if (((Point)list.get(position).item).getRodLengthId().equals("1")) {
+//                viewHolder.imageView.setVisibility(View.INVISIBLE);
+                viewHolder.imageView.setImageResource(R.drawable.ic_launcher);
+                viewHolder.imageView.getLayoutParams().width = 120;
+                viewHolder.imageView.getLayoutParams().height = 180;
+            } else {
+//                viewHolder.imageView.setVisibility(View.VISIBLE);
+//                viewHolder.imageView.setImageResource(R.drawable.ic_launcher);
+                viewHolder.imageView.getLayoutParams().width = 0;
+                viewHolder.imageView.getLayoutParams().height = 180;
+            }
+            viewHolder.titleTextView.setTag(list.get(position).item);//// 保存bean值到UI tag (响应事件从这个UI tag取值)
 
             return convertView;
         }
@@ -215,12 +162,12 @@ public class MainTab1Fragment extends Fragment implements OnClickListener {
          */
         private void addContentLayoutOnClickListener(final ViewHolder viewHolder, View convertView) {
             
-            RelativeLayout contentLayout = (RelativeLayout)convertView.findViewById(R.id.col1Layout);
+            LinearLayout contentLayout = (LinearLayout)convertView.findViewById(R.id.col1Layout);
             contentLayout.setOnClickListener(new OnClickListener() {
 
                 @Override
                 public void onClick(View arg0) {
-                    Point element = (Point)viewHolder.message_view.getTag();
+                    Point element = (Point)viewHolder.titleTextView.getTag();
                     Intent intent = new Intent(context, PlaceDetailActivity.class);
                 }
             });
