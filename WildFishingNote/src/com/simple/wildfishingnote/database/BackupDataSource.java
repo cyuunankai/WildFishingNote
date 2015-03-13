5f718ea69503fadfe9c0049965ecbfc99fd9969f
li_yang <li_yang@microad-tech.com>
2015-03-13 15:59:12 +0800

    add backup and restore

--- WildFishingNote/src/com/simple/wildfishingnote/database/BackupDataSource.java
+++ WildFishingNote/src/com/simple/wildfishingnote/database/BackupDataSource.java
@@ -50,21 +50,44 @@ public class BackupDataSource {
         database.execSQL(SQL_CREATE_BACKUPS);
     }
 
-    public long addBackup(Backup backup) {
-        
+    public void addBackup(Backup backup) {
+        String backupId = getBackupByPath(backup.getPath());
         // Create a new map of values, where column names are the keys
         ContentValues values = new ContentValues();
         values.put(WildFishingContract.Backups.COLUMN_NAME_PATH, backup.getPath());
         values.put(WildFishingContract.Backups.COLUMN_NAME_TIME, backup.getTime());
+        
+        if(backupId == null){
+            database.insert(
+                    WildFishingContract.Backups.TABLE_NAME,
+                    null,
+                    values);
+        }else{
+            String selection = WildFishingContract.Backups._ID + " = ? ";
+            String[] selelectionArgs = { String.valueOf(backupId) };
+            database.update(WildFishingContract.Backups.TABLE_NAME, values, selection, selelectionArgs);
+        }
+    }
+    
+    public String getBackupByPath(String path) {
+        Cursor cursor = database.query(WildFishingContract.Backups.TABLE_NAME,
+                backAllColumns, "path = '" + path + "'", null, null, null, WildFishingContract.Backups.COLUMN_NAME_TIME + " DESC");
 
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId;
-        newRowId = database.insert(
-                WildFishingContract.Backups.TABLE_NAME,
-                null,
-                values);
+        Backup obj = null;
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            obj = cursorToBackup(cursor);
+            cursor.moveToNext();
+            break;
+        }
+        // make sure to close the cursor
+        cursor.close();
 
-        return newRowId;
+        if (obj == null) {
+            return null;
+        } else {
+            return obj.getId();
+        }
     }
 
     public List<Backup> getBackups() {
@@ -76,7 +99,7 @@ public class BackupDataSource {
         int wantToFetchRecord = 0;
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            if (wantToFetchRecord == 3) {
+            if (wantToFetchRecord == 1) {
                 break;
             }
             Backup obj = cursorToBackup(cursor);
